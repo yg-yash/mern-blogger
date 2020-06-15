840608e073d7cba0089ab258ca90c3c0612cfb69
yg-yash <yg123yash@gmail.com>
2020-06-15 19:54:02 +0530

    added key

--- client/src/store/actions/user.js
+++ client/src/store/actions/user.js
@@ -3,56 +3,55 @@ import {
   SET_UNAUTHENTICATED,
   SET_ERROR,
   USER_LOADING,
-  CLEAR_ERROR
-} from "../types";
-import axios from "axios";
-import setAuthToken from "../../util/setAuthToken";
+  CLEAR_ERROR,
+} from '../types';
+import axios from 'axios';
+import setAuthToken from '../../util/setAuthToken';
 
-export const logIn = (userDetails, history) => async dispatch => {
+export const logIn = (userDetails, history) => async (dispatch) => {
   try {
-    const response = await axios.post("/user/login", userDetails);
+    const response = await axios.post('/user/login', userDetails);
     const { token } = response.data;
-    localStorage.setItem("jwtToken", token);
-    setAuthToken(token);
+    localStorage.setItem('jwtToken', token);
+    await setAuthToken(token);
     dispatch(getUserData());
     dispatch({ type: CLEAR_ERROR });
-    setTimeout(() => {
-      return history.push("/");
-    }, 100);
+
+    return history.push('/');
   } catch (error) {
     dispatch({ type: SET_ERROR, payload: error.response.data });
   }
 };
 
-export const signup = (userDetails, history) => async dispatch => {
+export const signup = (userDetails, history) => async (dispatch) => {
   try {
-    await axios.post("/user/signup", userDetails);
+    await axios.post('/user/signup', userDetails);
     dispatch({ type: CLEAR_ERROR });
-    history.push("/login");
+    history.push('/login');
   } catch (error) {
     dispatch({ type: SET_ERROR, payload: error.response.data });
   }
 };
 
-export const getUserData = () => async dispatch => {
+export const getUserData = () => async (dispatch) => {
   dispatch({ type: USER_LOADING });
   try {
-    const response = await axios.get("/user/current_user");
+    const response = await axios.get('/user/current_user');
     dispatch({ type: SET_USER, payload: response.data });
   } catch (error) {
     console.log(error);
   }
 };
-export const logout = history => async dispatch => {
-  localStorage.removeItem("jwtToken");
+export const logout = (history) => async (dispatch) => {
+  localStorage.removeItem('jwtToken');
   setAuthToken(false);
   dispatch({ type: SET_UNAUTHENTICATED });
-  history.push("/login");
+  history.push('/login');
 };
 
-export const editUser = editedUser => async (dispatch, getState) => {
+export const editUser = (editedUser) => async (dispatch, getState) => {
   try {
-    await axios.patch("user/edit", editedUser);
+    await axios.patch('user/edit', editedUser);
     dispatch({ type: USER_LOADING });
     dispatch(getUserData());
     dispatch({ type: CLEAR_ERROR });
