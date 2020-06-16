e902d8587433e5bb3df89263ce78f1a057486435
yg-yash <yg123yash@gmail.com>
2020-06-16 14:33:30 +0530

    fixed auth routes

--- utils/auth.js
+++ utils/auth.js
@@ -1,21 +1,26 @@
-const jwt = require("jsonwebtoken");
-const User = require("../models/User");
-const keys = require("../config/keys");
-module.exports = function(req, res, next) {
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+const keys = require('../config/keys');
+module.exports = function (req, res, next) {
   try {
-    if (req.headers && req.headers.authorization) {
-      var authorization = req.headers.authorization,
-        decoded;
-      decoded = jwt.verify(authorization, keys.secret);
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith('Bearer ')
+    ) {
+      const idToken = req.headers.authorization.split('Bearer ')[1];
+      // var authorization = req.headers.authorization,
+      const decoded = jwt.verify(idToken, keys.secret);
+
       var userId = decoded.id;
+
       // Fetch the user by id
-      User.findOne({ _id: userId }).then(function(user) {
+      User.findOne({ _id: userId }).then(function (user) {
         // Do something with the user
         req.user = user;
         return next();
       });
     } else {
-      return res.status(401).json({ error: "Unauthorized" });
+      return res.status(401).json({ error: 'Unauthorized' });
     }
   } catch (error) {
     return res.status(500).json({ error });
